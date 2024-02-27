96ec2c6fb8211d9e2958de61b26054a015e582f3
FerLedesma <ferledesma352@gmail.com>
2024-02-27 14:16:21 -0300

    Inicio Proyecto

---
+++ src/main/java/com/sistema/playaAutos/playaAutos/PlayaAutosApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.playaAutos.playaAutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayaAutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayaAutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/playaAutos/playaAutos/PlayaAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.playaAutos.playaAutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayaAutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
