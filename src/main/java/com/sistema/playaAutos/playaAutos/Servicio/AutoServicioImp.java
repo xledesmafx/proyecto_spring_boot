4922639feda7894e297a6e0b0e873eacf47fee58
FerLedesma <ferledesma352@gmail.com>
2024-03-05 15:51:53 -0300

    Creacion de los metodos rest en AutoServicioImp

--- src/main/java/com/sistema/playaAutos/playaAutos/Servicio/AutoServicioImp.java
+++ src/main/java/com/sistema/playaAutos/playaAutos/Servicio/AutoServicioImp.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class AutoServicioImp implements AutoServicio{
+public class AutoServicioImp implements AutoServicio {
 
     @Autowired  //EL CONTROLADOR UTILIZA LA CAPA DE INDIVIDUOSERVICIOIMP
     private vehiculoDao vehiculodao;
@@ -29,11 +31,34 @@ public class AutoServicioImp implements AutoServicio{
     @Override
     @Transactional
     public void borrar(Auto auto) {
-            vehiculodao.delete(auto);
-        }
+        vehiculodao.delete(auto);
+    }
 
     @Override
     public Auto localizarAuto(Auto auto) {
         return vehiculodao.findById(auto.getId()).orElse(null);
+    }
+
+    //----------------ESTO YA FORMA PARTE DEL API REST----------------------
+
+    public ArrayList<Auto> obteneraAutos() {
+        return (ArrayList<Auto>) vehiculodao.findAll();
+    }
+
+    public Auto guardarAutos(Auto auto) {
+        return vehiculodao.save(auto);
+    }
+
+    public Optional<Auto> obtenerPorId(Long id) {
+        return vehiculodao.findById(id);
+    }
+
+    public boolean eliminarVehiculo(Long id) {
+        try {
+            vehiculodao.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
