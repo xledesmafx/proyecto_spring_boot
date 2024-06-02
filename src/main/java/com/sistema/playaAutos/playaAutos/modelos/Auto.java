df05d4ed367e48c07974c06549fc7f1f9e88bdcc
xledesmafx <ferledesma352@gmail.com>
2024-06-02 19:52:02 -0400

    Update Auto.java
    Cambios varios

--- src/main/java/com/sistema/playaAutos/playaAutos/modelos/Auto.java
+++ src/main/java/com/sistema/playaAutos/playaAutos/modelos/Auto.java
@@ -13,6 +13,8 @@ public class Auto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    //HOLA MUNDO DESDE GTHUBDESKTOP
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
