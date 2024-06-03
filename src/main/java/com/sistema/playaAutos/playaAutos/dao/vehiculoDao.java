2f2267775774fb3af30a56523a9ccc3f4ba3b7dd
xledesmafx <124525649+xledesmafx@users.noreply.github.com>
2024-06-02 20:00:20 -0400

    Cambios en vehiculo dao desde github desktop

--- src/main/java/com/sistema/playaAutos/playaAutos/dao/vehiculoDao.java
+++ src/main/java/com/sistema/playaAutos/playaAutos/dao/vehiculoDao.java
@@ -3,5 +3,7 @@ package com.sistema.playaAutos.playaAutos.dao;
 import com.sistema.playaAutos.playaAutos.modelos.Auto;
 import org.springframework.data.repository.CrudRepository;
 
+//HOLA MUNDO DESDE EL GITHUB WEB
+
 public interface vehiculoDao extends CrudRepository<Auto, Long> {
 }
